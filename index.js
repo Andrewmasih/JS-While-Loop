03f077c9b08e941af8e7c950dc2e80a9b3f63b8f
Andrew Masih <armasih@outlook.com>
2021-06-28 21:45:21 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/* use the keyword 'while' and set the condition in the parentheses(), add code to be executed in the curley brackets {} 
+
+A 'while loop' allows us to runcode while the condition in the parentheses is TRUE, if it is not TRUE, it stops the iteration. */
+
+let i = 0;
+
+while (i < 10) {
+
+  console.log ("I am a while loop");
+
+  i ++;
+
+}
+
+
+
+
+
