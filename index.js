ce642e2a938dbf9206bc5efb4a38b00275ecbf0a
Andrew Masih <armasih@outlook.com>
2021-06-29 23:46:28 +0000

    added commets

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* use the keyword 'while' and set the condition in the parentheses(), add code to be executed in the curley brackets {} 
 
-A 'while loop' allows us to runcode while the condition in the parentheses is TRUE, if it is not TRUE, it stops the iteration. */
+A 'while loop' allows us to run code while the condition in the parentheses is TRUE, if it is not TRUE, it stops the iteration. */
 
 let i = 0;
 
